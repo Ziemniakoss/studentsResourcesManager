f9eef382f6eb7b8394cf6d0c2dca07a3f6ae9f66
ZIemniakoss <rawa.przemek@gmail.com>
2020-03-15 22:32:53 +0100

    Initial commit

---
+++ src/main/java/pl/ziemniakoss/studentsresourcesmanager/StudentsResourcesManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.ziemniakoss.studentsresourcesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsResourcesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsResourcesManagerApplication.class, args);
+	}
+
+}
