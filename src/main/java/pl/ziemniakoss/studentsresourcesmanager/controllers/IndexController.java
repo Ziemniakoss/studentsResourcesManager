3781b90ffcbadc8bfe2f72633aa461650da4459f
ZIemniakoss <rawa.przemek@gmail.com>
2020-04-14 15:35:43 +0200

    nawigacja  po stronach głównych
    Strony główne dla każdej roli, na razie puste

--- src/main/java/pl/ziemniakoss/studentsresourcesmanager/controllers/IndexController.java
+++ src/main/java/pl/ziemniakoss/studentsresourcesmanager/controllers/IndexController.java
@@ -2,16 +2,38 @@ package pl.ziemniakoss.studentsresourcesmanager.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.annotation.security.RolesAllowed;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class IndexController {
 	@GetMapping("/")
 	public String index() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth == null) {
+			return "redirect:/login";
+		}
+		List<? extends GrantedAuthority> roles = auth.getAuthorities().stream().filter(e -> e.getAuthority().startsWith("ROLE_")).collect(Collectors.toList());
+		if(roles.size() > 1){
+			return "common_home";
+		}
+		switch (roles.get(0).getAuthority()){
+			case "ROLE_ADMIN":
+				return "admin-home";
+			case "ROLE_STUDENT":
+				return "student-home";
+			case "ROLE_EMPLOYEE":
+				return "employee-home";
+		}
+
+
 		return "index";
 	}
 }
---
+++ src/main/java/pl/ziemniakoss/studentsresourcesmanager/controllers/admin/AdminHomeController.java
@@ -0,0 +1,17 @@
+package pl.ziemniakoss.studentsresourcesmanager.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.security.RolesAllowed;
+
+@Controller
+public class AdminHomeController {
+
+	@GetMapping("/res-admin")
+	@RolesAllowed("ADMIN")
+	public String home(){
+		return "admin-home";
+
+	}
+}
