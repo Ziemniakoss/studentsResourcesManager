4ffec86ce927314ca0f6490cbda7e85d22fcc652
ZIemniakoss <rawa.przemek@gmail.com>
2020-04-16 22:33:18 +0200

    emaile: więcej testóœ i nowy regex

--- src/test/java/pl/ziemniakoss/studentsresourcesmanager/utils/EmailUtilsTest.java
+++ src/test/java/pl/ziemniakoss/studentsresourcesmanager/utils/EmailUtilsTest.java
@@ -13,7 +13,9 @@ class EmailUtilsTest {
 	@ValueSource(strings = {
 			"trawa@gmail.com",
 			"gwiazda@pw.edu.pl",
-			"trawa.prawa@pw.edu.pl"
+			"trawa.prawa@pw.edu.pl",
+//			"ulalal@50.128.210.78", NA razie tego nie wspieramy wgl
+//			"matteo@78.47.122.114"
 	})
 	void correctEmail(String s) {
 		assertTrue(emailUtils.isEmail(s));
@@ -25,8 +27,17 @@ class EmailUtilsTest {
 			"",
 			"              ",
 			"gwiazda@pl",
-
-
+			"aaa aa@pw.ed.pl",
+			"gaghg\tafsa@pw.edu.pl",
+			"bar@",
+			"adasfa@.pl",
+			"pusty",
+			"@gmail.pl",
+			" @yahoo.com",
+			"adsafa@yak@kek.pl",
+			"sfafafafa ala@pw.edu.pl asaifhah",
+			"sfafafafa ala@pw.edu.pl asaifhah",
+			"ala@p w.edu.pl"
 	})
 	void incorrectEmail(String s) {
 		assertFalse(emailUtils.isEmail(s));
